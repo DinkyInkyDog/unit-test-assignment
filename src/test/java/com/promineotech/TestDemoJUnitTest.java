6f11e698cc0fbea68299d758c3edfac43a71fd67
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-06-22 19:49:33 -0400

    setup addPositive test

--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -1,9 +1,11 @@
 package com.promineotech;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class TestDemoJUnitTest {
 private TestDemo td;
@@ -12,9 +14,25 @@ private TestDemo td;
 		td = new TestDemo();
 	}
 
-	@Test
-	void test() {
-		
+	@ParameterizedTest
+	@MethodSource()
+	void assertThatAddPostiveWorks(int a, int b, int expected, boolean expectException) {
+		//Given: two numbers
+		//when: if the numbers are positive, you add the two numbers together
+		if(!expectException) {
+		//then: return the sum of the two positive numbers
+			
+		}
+		//when: if the numbers are negative or zero, you don't add the numbers
+		//then: throw an IllegalArgumentException.
 	}
 
+private Stream<Arguments> addPositiveArguments() {
+	//formatter:off
+	return Stream.of(
+			arguments()
+	);
+	//formatter:on
+}
+	
 }
