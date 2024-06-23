25146164dfee0fc54cdbe2a58d4188f76f0798d1
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-06-22 20:00:06 -0400

    delete unneeded exception finish test code
    I realized that IllegalArgumentException is an actual exception that I don't need to create myself. So I got rid of it. Then I finished up the test code. Time to test it and see that good old red bar.

--- src/main/java/com/promineotech/TdException.java
+++
@@ -1,5 +0,0 @@
-package com.promineotech;
-
-public class TdException extends RuntimeException {
-
-}
--- src/test/java/com/promineotech/TestDemoJUnitTest.java
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -5,6 +5,10 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.*;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class TestDemoJUnitTest {
@@ -18,13 +22,15 @@ private TestDemo td;
 	@MethodSource()
 	void assertThatAddPostiveWorks(int a, int b, int expected, boolean expectException) {
 		//Given: two numbers
-		//when: if the numbers are positive, you add the two numbers together
+		
 		if(!expectException) {
-		//then: return the sum of the two positive numbers
-			
+			//when: if the numbers are positive, you add the two numbers together
+			//then: return the sum of the two positive numbers
+			assertThat(td.addPositive(a, b)).isEqualTo(expected);
 		}
 		//when: if the numbers are negative or zero, you don't add the numbers
 		//then: throw an IllegalArgumentException.
+		assertThatThrownBy(() -> td.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
 	}
 
 private Stream<Arguments> addPositiveArguments() {
